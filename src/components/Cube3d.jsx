38f70277b1e6c09ebf1946a5750960ff9b427da8
EthanPaleyron <ethan.paleyron@gmail.com>
2025-01-22 16:38:12 +0100

    feat : header

--- src/components/Cube3d.jsx
+++ src/components/Cube3d.jsx
@@ -1,25 +1,25 @@
 import gsap from "gsap";
 
 gsap.fromTo(
-  ".cube",
-  {
-    x: -800,
-    y: -50,
-    rotationX: 170,
-    rotationY: 80,
-  },
-  {
-    x: 0,
-    y: 0,
-    rotationX: -15,
-    rotationY: 60,
-    duration: 1.5,
-    ease: "power2.out",
-    onComplete: () => {
-      addMouseEffect();
-      addScrollEffect();
+    ".cube",
+    {
+        x: -800,
+        y: -50,
+        rotationX: 170,
+        rotationY: 80,
     },
-  }
+    {
+        x: 0,
+        y: 0,
+        rotationX: -15,
+        rotationY: 60,
+        duration: 1.5,
+        ease: "power2.out",
+        onComplete: () => {
+            addMouseEffect();
+            addScrollEffect();
+        },
+    }
 );
 
 let mouseRotationX = -15;
@@ -28,49 +28,51 @@ let scrollRotationX = 0;
 let scrollRotationY = 0;
 
 function addMouseEffect() {
-  window.addEventListener("mousemove", (e) => {
-    const bodyWidth = document.body.scrollWidth;
-    const bodyHeight = document.body.scrollHeight;
-    const mouseX = e.pageX;
-    const mouseY = e.pageY;
+    window.addEventListener("mousemove", (e) => {
+        const bodyWidth = document.body.scrollWidth;
+        const bodyHeight = document.body.scrollHeight;
+        const mouseX = e.pageX;
+        const mouseY = e.pageY;
 
-    mouseRotationY = (mouseX / bodyWidth) * 35 - 10;
-    mouseRotationX = (mouseY / bodyHeight) * -35 + 10;
-    updateCubeRotation();
-  });
+        mouseRotationY = (mouseX / bodyWidth) * 35 - 10;
+        mouseRotationX = (mouseY / bodyHeight) * -35 + 10;
+        updateCubeRotation();
+    });
 }
 
 function addScrollEffect() {
-  const maxScroll = document.body.scrollHeight - window.innerHeight;
+    const maxScroll = document.body.scrollHeight - window.innerHeight;
 
-  window.addEventListener("scroll", () => {
-    const scrollPosition = window.scrollY;
-    const scrollPercent = scrollPosition / maxScroll;
+    window.addEventListener("scroll", () => {
+        const scrollPosition = window.scrollY;
+        const scrollPercent = scrollPosition / maxScroll;
 
-    scrollRotationY = scrollPercent * -120;
-    scrollRotationX = scrollPercent * -180;
-    updateCubeRotation();
-  });
+        scrollRotationY = scrollPercent * -120;
+        scrollRotationX = scrollPercent * -180;
+        updateCubeRotation();
+    });
 }
 
 function updateCubeRotation() {
-  gsap.to(".cube", {
-    rotationX: mouseRotationX + scrollRotationX - 15,
-    rotationY: mouseRotationY + scrollRotationY + 60,
-    duration: 0.3,
-    ease: "power2.out",
-  });
+    gsap.to(".cube", {
+        rotationX: mouseRotationX + scrollRotationX - 15,
+        rotationY: mouseRotationY + scrollRotationY + 60,
+        duration: 0.3,
+        ease: "power2.out",
+    });
 }
 
 export default function Cube3d() {
-  return (
-    <div className="cube">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
+    return (
+        <div className={"content-cube"}>
+            <div className={"cube"}>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
 }
--- src/routes/index.lazy.jsx
+++ src/routes/index.lazy.jsx
@@ -1,14 +1,23 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import {createLazyFileRoute} from "@tanstack/react-router";
+import "../sass/homepage.scss"
 import Cube3d from "../components/Cube3d";
 
 export const Route = createLazyFileRoute("/")({
-  component: Index,
+    component: Index,
 });
 
 function Index() {
-  return (
-    <main>
-      
-    </main>
-  );
+    return (
+        <main className={"homepage"}>
+            <header className={"header"}>
+                <div className={"header__container container"}>
+                    <Cube3d></Cube3d>
+                    <div className={"header__container__content"}>
+                        <h1>Ethan<br/>Paleyron</h1>
+                        <p>Développeur web fullstack basé près de Lyon, je suis à la recherche d'une alternance.</p>
+                    </div>
+                </div>
+            </header>
+        </main>
+    );
 }
