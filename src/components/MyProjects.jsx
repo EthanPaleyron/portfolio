2f58b92bcbba1fe0356619e77489f10cb29a5453
EthanPaleyron <ethan.paleyron@gmail.com>
2025-01-23 14:41:07 +0100

    feat : json projectsList

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -1,11 +1,41 @@
 import CardProject from "./CardProject.jsx";
+import { useState, useEffect } from "react";
+import { projectsList } from "../data/projectsList.js";
 
 export default function MyProjects() {
+  const [projects, setProjects] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  async function fetchData() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // Filter projects where toMyProjects is true
+    const filteredProjects = projectsList.filter(
+      (project) => project.toMyProjects
+    );
+    setProjects(filteredProjects);
+    setLoader(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <article className={"my-projects__projects"}>
-      <CardProject></CardProject>
-      <CardProject></CardProject>
-      <CardProject></CardProject>
+    <article className={"my-projects__container__projects"}>
+      {loader ? (
+        <div>Chargement des projets...</div>
+      ) : (
+        projects.map((project) => (
+          <CardProject
+            key={project.id}
+            skills={project.skills}
+            image={project.image}
+            to={`/project/${project.id}`}
+          >
+            {project.name}
+          </CardProject>
+        ))
+      )}
     </article>
   );
 }
---
+++ src/data/projectsList.js
@@ -0,0 +1,53 @@
+export const projectsList = [
+  {
+    id: 1,
+    name: "CCA",
+    skills: ["PHP", "SASS", "Javascript", "SQL"],
+    description:
+      "Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.",
+    buttons: [
+      {
+        link: "https://www.cca-rillieux-vancia.fr",
+        color: "yellow",
+        content: "CCA Rillieux Vancia",
+      },
+    ],
+    image: "cca.jpg",
+    year: 2024,
+    toMyProjects: true,
+  },
+  {
+    id: 2,
+    name: "CCA",
+    skills: ["PHP", "SASS", "Javascript", "SQL"],
+    description:
+      "Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.",
+    buttons: [
+      {
+        link: "https://www.cca-rillieux-vancia.fr",
+        color: "yellow",
+        content: "CCA Rillieux Vancia",
+      },
+    ],
+    image: "cca.jpg",
+    year: 2024,
+    toMyProjects: true,
+  },
+  {
+    id: 3,
+    name: "CCA",
+    skills: ["PHP", "SASS", "Javascript", "SQL"],
+    description:
+      "Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.",
+    buttons: [
+      {
+        link: "https://www.cca-rillieux-vancia.fr",
+        color: "yellow",
+        content: "CCA Rillieux Vancia",
+      },
+    ],
+    image: "cca.jpg",
+    year: 2024,
+    toMyProjects: false,
+  },
+];
