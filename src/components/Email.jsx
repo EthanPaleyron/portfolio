a469bde781b321418ec88bc2944a0a542d9cd0f9
EthanPaleyron <ethan.paleyron@gmail.com>
2025-01-25 00:06:31 +0100

    feat : copy email and dynamic social media

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,8 +1,26 @@
 export default function Email() {
+  const mail = "ethan.paleyron@proton.me";
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(mail)
+      .then(() => {
+        // Optional: Add a visual feedback like a toast or temporary highlight
+        console.log("Email copied to clipboard");
+      })
+      .catch((err) => {
+        console.error("Failed to copy email", err);
+      });
+  };
+
   return (
-    <button className="email">
-      <b>paleyron.ethan.05@gmail.com</b>
-      <img src="../../public/img/icons/copy.svg" alt="Copier" />
-    </button>
+    <div className="email">
+      <a href={`mailto:${mail}`} className="link">
+        {mail}
+      </a>
+      <button id="copy" onClick={handleCopy}>
+        <img src="/img/icons/copy.svg" alt="Copier" />
+      </button>
+    </div>
   );
 }
--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,15 +1,32 @@
 export default function SocialMedia() {
+  const socialMedia = [
+    {
+      title: "Github",
+      link: "https://github.com/EthanPaleyron",
+    },
+    {
+      title: "Linkedin",
+      link: "https://www.linkedin.com/in/ethan-paleyron-4456092b2/",
+    },
+    {
+      title: "Instagram",
+      link: "https://www.instagram.com/ethanp._05/",
+    },
+  ];
   return (
     <ul className="social-media">
-      <li>
-        <a href="#">GitHub</a>
-      </li>
-      <li>
-        <a href="#">Linkedin</a>
-      </li>
-      <li>
-        <a href="#">Instagram</a>
-      </li>
+      {socialMedia.map((social, index) => (
+        <li key={index}>
+          <a
+            href={social.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link"
+          >
+            {social.title}
+          </a>
+        </li>
+      ))}
     </ul>
   );
 }
