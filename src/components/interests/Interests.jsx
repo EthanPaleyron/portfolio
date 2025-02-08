e0e0ab6ddb6c92f7456013e59ecfaa6f727e16c6
EthanPaleyron <ethan.paleyron@gmail.com>
2025-02-08 16:20:08 +0100

    fix : contents interests

--- src/components/interests/Interests.jsx
+++ src/components/interests/Interests.jsx
@@ -4,21 +4,21 @@ export default function Interests() {
   return (
     <div className="interests">
       <ul className="interests__content">
-        <Item image={"revision.jpg"}>Escalade</Item>
+        <Item image={"revision.jpg"}>Voyager</Item>
         <Item image={"revision.jpg"}>Ski</Item>
-        <Item image={"revision.jpg"}>Voyager</Item>
-        <Item image={"revision.jpg"}>Sensations forte</Item>
+        <Item image={"revision.jpg"}>Escalade</Item>
         <Item image={"revision.jpg"}>Plongée</Item>
-        <Item image={"revision.jpg"}>Centre d'interêt x</Item>
-        <Item image={"cca-rillieux-vancia.png"}>Musique</Item>
-        <Item image={"revision.jpg"}>Voyager</Item>
+        <Item image={"revision.jpg"}>Sensations forte</Item>
+        <Item image={"revision.jpg"}>Escape Game</Item>
+        <Item image={"revision.jpg"}>Musique</Item>
         <Item image={"revision.jpg"}>Echecs</Item>
+        <Item image={"revision.jpg"}>Voyager</Item>
         <Item image={"revision.jpg"}>Lire</Item>
         <Item image={"revision.jpg"}>Jeux vidéo</Item>
         <Item image={"revision.jpg"}>Programmation</Item>
-        <Item image={"revision.jpg"}>Boxe</Item>
-        <Item image={"revision.jpg"}>L'espace</Item>
-        <Item image={"revision.jpg"}>Mon chien</Item>
+        <Item image={"revision.jpg"}>Aventures</Item>
+        <Item image={"revision.jpg"}>Astronomie</Item>
+        <Item image={"revision.jpg"}>Parc d'attraction</Item>
       </ul>
     </div>
   );
