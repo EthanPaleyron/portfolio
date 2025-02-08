cbf3c3c496381d91fbb8bb539f3fd029bf3d8da1
EthanPaleyron <ethan.paleyron@gmail.com>
2025-02-08 01:04:30 +0100

    feat : contents projects

--- src/data/projectsList.js
+++ src/data/projectsList.js
@@ -1,58 +1,79 @@
 export const projectsList = [
   {
     id: 1,
-    name: "CCA Rillieux Vancia",
-    skills: ["PHP", "SASS", "Javascript", "SQL"],
-    projectObjective:
-      "Le but de ce projet était de créer une plateforme détailler l'objectif, ex : e-commerce, portfolio, blog, gestion de tâches permettant aux utilisateurs de fonction principale, ex : acheter des produits, afficher des travaux professionnels, suivre des tâches quotidiennes. L'accent a été mis sur l'ergonomie, la performance et une expérience utilisateur fluide.",
-    experienceGaind:
-      "Au cours de ce projet, j'ai acquis des compétences en développement Fullstack en créant une application complète, de la base de données à l'interface utilisateur. J'ai aussi amélioré ma maîtrise de l'intégration continue en automatisant les tests et le déploiement. Ce projet m'a permis de renforcer mon expertise en conception UX/UI en optimisant le parcours utilisateur pour une meilleure rétention. De plus, j'ai pu perfectionner ma collaboration en équipe à travers des pull requests et des revues de code sur GitHub.",
+    name: `CCA Rillieux Vancia`,
+    skills: [`PHP`, `SASS`, `Javascript`, `SQL`, `Figma`],
+    projectObjective: `Ce site web a été conçu pour le club de canine pour ma mère. L'objectif était de créer une plateforme intuitive et accessible, permettant aux utilisateurs de trouver facilement les informations essentielles sur le club`,
+    experienceGaind: `Ce projet m'a permis de comprendre à quel point une bonne organisation du code et une interface utilisateur bien pensée sont essentielles. J'ai également appris à développer un back-office sécurisé en PHP avec du SQL pour la gestion du site. De plus, j'ai acquis de nouvelles compétences en hébergement web, en assurant la mise en ligne et la maintenance du site.`,
     buttons: [
       {
-        link: "https://www.cca-rillieux-vancia.fr",
-        color: "gold",
-        content: "Site web",
+        link: `https://www.cca-rillieux-vancia.fr`,
+        color: `gold`,
+        content: `Site web`,
       },
     ],
-    image: "cca-rillieux-vancia.png",
+    image: `cca-rillieux-vancia.webp`,
     year: 2024,
     toMyProjects: true,
   },
   {
     id: 2,
-    name: "Générateur de Kahoot",
-    skills: ["PHP", "SASS", "Javascript", "SQL"],
-    projectObjective:
-      "Le but de ce projet était de créer une plateforme détailler l'objectif, ex : e-commerce, portfolio, blog, gestion de tâches permettant aux utilisateurs de fonction principale, ex : acheter des produits, afficher des travaux professionnels, suivre des tâches quotidiennes. L'accent a été mis sur l'ergonomie, la performance et une expérience utilisateur fluide.",
-    experienceGaind:
-      "Au cours de ce projet, j'ai acquis des compétences en développement Fullstack en créant une application complète, de la base de données à l'interface utilisateur. J'ai aussi amélioré ma maîtrise de l'intégration continue en automatisant les tests et le déploiement. Ce projet m'a permis de renforcer mon expertise en conception UX/UI en optimisant le parcours utilisateur pour une meilleure rétention. De plus, j'ai pu perfectionner ma collaboration en équipe à travers des pull requests et des revues de code sur GitHub.",
+    name: `Extension quick notes`,
+    skills: [`Javascript`, `HTML`, `CSS`],
+    projectObjective: `L'objectif est de permettre de prendre des notes rapidement tout en restant sur la même page.`,
+    experienceGaind: `Créer mon extension web m'a appris à structurer un projet, faire des choix techniques et optimiser l'expérience utilisateur. J'ai renforcé mes compétences en front-end et gestion du stockage local. Voir mon idée devenir un outil fonctionnel a été une grande satisfaction.`,
     buttons: [
       {
-        link: "#",
-        color: "gold",
-        content: "Site web",
+        link: `#`,
+        color: `gold`,
+        content: `Download Extension`,
+      },
+      {
+        link: `https://github.com/EthanPaleyron/extension-quick-notes`,
+        color: `purple`,
+        content: `Code source`,
       },
     ],
-    image: "cca-rillieux-vancia.png",
+    image: `extension-quick-notes.webp`,
     year: 2024,
     toMyProjects: true,
   },
   {
     id: 3,
-    name: "Link in bio",
-    skills: ["Tailwind CSS", "Javascript"],
-    projectObjective:
-      "Le but de ce projet était de créer une plateforme détailler l'objectif, ex : e-commerce, portfolio, blog, gestion de tâches permettant aux utilisateurs de fonction principale, ex : acheter des produits, afficher des travaux professionnels, suivre des tâches quotidiennes. L'accent a été mis sur l'ergonomie, la performance et une expérience utilisateur fluide.",
-    experienceGaind:
-      "Au cours de ce projet, j'ai acquis des compétences en développement Fullstack en créant une application complète, de la base de données à l'interface utilisateur. J'ai aussi amélioré ma maîtrise de l'intégration continue en automatisant les tests et le déploiement. Ce projet m'a permis de renforcer mon expertise en conception UX/UI en optimisant le parcours utilisateur pour une meilleure rétention. De plus, j'ai pu perfectionner ma collaboration en équipe à travers des pull requests et des revues de code sur GitHub.",
+    name: `Générateur de Kahoot`,
+    skills: [`PHP`, `SASS`, `Javascript`, `SQL`, `Git`],
+    projectObjective: `Ce projet vise à offrir aux étudiants et formateurs un moyen rapide et efficace de créer des quiz Kahoot personnalisés. En saisissant un thème et des paramètres spécifiques, l'application génère automatiquement un quiz via ChatGPT, entièrement personnalisable et téléchargeable pour être exporté sur le site officiel de Kahoot. L'objectif est de simplifier la création de quiz tout en garantissant une flexibilité maximale.`,
+    experienceGaind: `Ce projet fil rouge, réalisé en équipe de quatre, m'a permis d'explorer l'ensemble du cycle de développement d'une application. Nous avons mené une étude approfondie des besoins via des questionnaires, défini les personas et les cibles, conçu l'UI/UX (wireframes et maquettes) et développé l'application en suivant la méthodologie Scrum Agile avec GitHub Projects, modélisation MCD/MLD/MPD, résolution de bugs et recette. Enfin, ce projet a été présenté devant un jury pour l'évaluation de fin d'année, consolidant mon expérience en gestion de projet et en présentation professionnelle. (Il nous reste plus qu'à corriger les derniers problèmes et à l'héberger. 🙃)`,
     buttons: [
       {
-        link: "https://ethanpaleyron.github.io/",
-        color: "gold",
-        content: "Site web",
+        link: `#`,
+        color: `gold`,
+        content: `Site web`,
       },
     ],
-    image: "cca-rillieux-vancia.png",
+    image: `generateur-de-kahoot.webp`,
+    year: 2024,
+    toMyProjects: true,
+  },
+  {
+    id: 4,
+    name: `Link in bio`,
+    skills: [`Figma UI / UX`, `Tailwind CSS`, `Javascript`],
+    projectObjective: `Ce projet "Link in Bio" a pour but de créer une page web simple et efficace permettant de centraliser mes informations essentielles. Elle présente brièvement qui je suis, mes passions et mes réseaux sociaux. L'objectif est d'offrir une expérience fluide et accessible aux visiteurs, avec une interface optimisée pour tous les supports.`,
+    experienceGaind: `Ce projet m'a permis de renforcer mes compétences en accessibilité et en design UI/UX. J'ai travaillé sur une interface claire et intuitive, en respectant les bonnes pratiques d'ergonomie et de conception. L'exercice m'a également appris à mieux structurer mon travail sur Figma et à anticiper les besoins des utilisateurs pour améliorer leur expérience de navigation.`,
+    buttons: [
+      {
+        link: `https://ethanpaleyron.github.io/`,
+        color: `gold`,
+        content: `Site web`,
+      },
+      {
+        link: `https://github.com/EthanPaleyron/EthanPaleyron.github.io`,
+        color: `purple`,
+        content: `Code source`,
+      },
+    ],
+    image: `link-in-bio.webp`,
     year: 2024,
     toMyProjects: true,
   },
