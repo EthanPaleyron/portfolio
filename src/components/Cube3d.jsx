66ae27f34d49fba9fdff0dfb3f9619f5218bcb38
EthanPaleyron <ethan.paleyron@gmail.com>
2025-02-16 23:05:05 +0100

    fix : cube animation

--- src/components/Cube3d.jsx
+++ src/components/Cube3d.jsx
@@ -1,30 +1,36 @@
 import React, { useEffect } from "react";
 import { motion, useSpring, useMotionValue, animate } from "motion/react";
 
-const Cube3d = () => {
-  const rotateX = useSpring(0, { damping: 20, stiffness: 400 });
-  const rotateY = useSpring(0, { damping: 20, stiffness: 400 });
-  const x = useMotionValue(0);
-  const y = useMotionValue(0);
+export default function Cube3d() {
+  const rotateX = useSpring(0, { damping: 50, stiffness: 1000 });
+  const rotateY = useSpring(0, { damping: 50, stiffness: 1000 });
+  const rotateZ = useMotionValue(0);
+  const x = useMotionValue(-800);
+  const y = useMotionValue(-200);
 
   useEffect(() => {
-    animate({ x: ["-800px", "0px"] }, { duration: 0 });
-    animate({ y: ["-50px", "0px"] }, { duration: 0 });
-
     animate(x, 0, {
-      duration: 1.5,
+      duration: 1.25,
       ease: "easeOut",
     });
     animate(y, 0, {
-      duration: 1.5,
+      duration: 1.25,
       ease: "easeOut",
     });
+
+    animate(rotateZ, 190, {
+      duration: 1.25,
+      ease: "easeOut",
+    });
+
+    rotateY.set(-30);
+    rotateX.set(-15);
 
     const handleMouseMove = (e) => {
       const mouseX = (e.clientX / window.innerWidth) * 35 - 10;
       const mouseY = (e.clientY / window.innerHeight) * -35 + 10;
 
-      rotateY.set(mouseX + 60);
+      rotateY.set(mouseX - 30);
       rotateX.set(mouseY - 15);
     };
 
@@ -43,6 +49,7 @@ const Cube3d = () => {
           y,
           rotateX,
           rotateY,
+          rotateZ,
         }}
       >
         <div></div>
@@ -54,6 +61,4 @@ const Cube3d = () => {
       </motion.div>
     </div>
   );
-};
-
-export default Cube3d;
+}
