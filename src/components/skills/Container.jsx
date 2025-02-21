e3f83befcb79923abb384ba6e543ebd8487f8b93
EthanPaleyron <ethan.paleyron@gmail.com>
2025-02-21 13:39:54 +0100

    fix : skills

--- src/components/skills/Container.jsx
+++ src/components/skills/Container.jsx
@@ -2,11 +2,11 @@ import Skill from "./Skill";
 
 export default function Container({ title, skills }) {
   return (
-    <div className="about-me__skills__content__containers-skills__container">
-      <h3 className="container">{title}</h3>
-      <div className="about-me__skills__content__containers-skills__container__carousel container">
+    <div className="about-me__skills__content__containers-skills__container container">
+      <h3>{title}</h3>
+      <div className="about-me__skills__content__containers-skills__container__carousel">
         {skills.map((skill, index) => (
-          <Skill key={index} icon={skill.icon}>
+          <Skill key={index} icon={skill.icon} source={skill.source}>
             {skill.title}
           </Skill>
         ))}
--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -1,8 +1,24 @@
-export default function Skill({ children, icon }) {
-  return (
-    <div className="about-me__skills__content__containers-skills__container__carousel__tag">
+export default function Skill({ children, source, icon }) {
+  const isClickable = Boolean(source);
+
+  const content = (
+    <>
       {icon && <img src={icon} alt={`Logo ${children}`} title={children} />}
       <p>{children}</p>
-    </div>
+    </>
+  );
+
+  return isClickable ? (
+    <a
+      href={source}
+      target="_blank"
+      className="about-me__skills__content__containers-skills__container__carousel__tag"
+    >
+      {content}
+    </a>
+  ) : (
+    <span className="about-me__skills__content__containers-skills__container__carousel__tag">
+      {content}
+    </span>
   );
 }
