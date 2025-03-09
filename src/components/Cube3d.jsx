2d683bcc48d905fcf130bcf4cc4fbbdc44096c71
EthanPaleyron <ethan.paleyron@gmail.com>
2025-03-09 17:23:52 +0100

    fix : cube

--- src/components/Cube3d.jsx
+++ src/components/Cube3d.jsx
@@ -1,27 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { motion, useSpring, useMotionValue, animate } from "motion/react";
 
 export default function Cube3d() {
   const rotateX = useSpring(0, { damping: 50, stiffness: 1000 });
   const rotateY = useSpring(0, { damping: 50, stiffness: 1000 });
   const rotateZ = useMotionValue(0);
-  const x = useMotionValue(-800);
+  const x = useMotionValue(-1250);
   const y = useMotionValue(-500);
+  const [animationComplete, setAnimationComplete] = useState(false);
 
+  function startAnimation(timeout) {
+    setTimeout(() => {
+      animate(x, 0, {
+        duration: 1.25,
+        ease: "easeOut",
+      });
+      animate(y, 0, {
+        duration: 1.15,
+        ease: "easeOut",
+      });
+
+      animate(rotateZ, 2 * 90 + 10, {
+        duration: 1.25,
+        ease: "easeOut",
+        onComplete: () => {
+          setAnimationComplete(true);
+        },
+      });
+    }, timeout);
+  }
   useEffect(() => {
-    animate(x, 0, {
-      duration: 1.25,
-      ease: "easeOut",
-    });
-    animate(y, 0, {
-      duration: 1.15,
-      ease: "easeOut",
-    });
-
-    animate(rotateZ, 2 * 90 + 10, {
-      duration: 1.25,
-      ease: "easeOut",
-    });
+    startAnimation(100);
 
     rotateY.set(-30);
     rotateX.set(-15);
@@ -34,11 +43,28 @@ export default function Cube3d() {
       rotateX.set(mouseY - 15);
     };
 
-    window.addEventListener("mousemove", handleMouseMove);
+    if (animationComplete) {
+      window.addEventListener("mousemove", handleMouseMove);
+    }
+
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
-  }, []);
+  }, [animationComplete]);
+
+  const handleClick = () => {
+    setAnimationComplete(false);
+    x.set(-1250);
+    y.set(-500);
+    rotateZ.set(0);
+    rotateY.set(0);
+    rotateX.set(0);
+
+    startAnimation();
+
+    rotateY.set(-30);
+    rotateX.set(-15);
+  };
 
   return (
     <div className="content-cube">
@@ -51,6 +77,7 @@ export default function Cube3d() {
           rotateY,
           rotateZ,
         }}
+        onClick={handleClick}
       >
         <div></div>
         <div></div>
